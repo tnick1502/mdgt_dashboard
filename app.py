8fa5847947d1c15ec2867c58eedcac613b9871ea
tnick1502 <59287059+tnick1502@users.noreply.github.com>
2022-05-30 12:22:04 +0300

    Добавлена возможность использовать перекрестные запросы (для реакт)

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api import router
 
 app = FastAPI(
     title="DashBoard MDGT",
     description="Отображение показателей работы компании",
     version="1.0.0"
+)
+
+origins = [
+    "http://localhost:3000",
+    "localhost:3000"
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
 )
 
 app.include_router(router)
