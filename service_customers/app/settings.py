020beb426ad12d19956b4222f23c51ca4cdb7453
“tnick_linux” <tnick1502@mail.ru>
2022-07-01 13:18:19 +0300

    Новая конфигурация docker-compose

--- service_customers/app/settings.py
+++ service_customers/app/settings.py
@@ -6,10 +6,10 @@ customers = "/databases/customers/"
 
 class Settings(BaseSettings):
     server_host: str = "0.0.0.0"
-    server_port: int = 9600
+    server_port: int = 9000
 
     photo_path: str = f'{customers}photos'
     excel_file: str = f'{customers}customers.xlsx'
-    database_url: str = f'sqlite:///{customers}customers.sqlite3'
+    database_url: str = f"sqlite:///{customers}database.sqlite3"
 
 settings = Settings()
